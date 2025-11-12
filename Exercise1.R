a4b6d39e711f3d989f97c7288f32ba576923f9ce
Thor Hogerbrugge <thorhogerbrugge@Thors-MacBook-Air.local>
2025-11-12 15:26:14 +0100

    1a is af

--- Exercise1.R
+++ Exercise1.R
@@ -2,7 +2,6 @@
 # First one has to download the data from the 'Data.R' file
 
 ### 1a
-
 y <- estimation_data$y
 mu <- mean(y)
 std <- sd(y)
@@ -10,14 +9,28 @@ p1 <- 0.5
 sigma_1 <- 0.5 * std
 sigma_2 <- std
 
-nll <- function(mu, sigma_1, sigma_2, p1, y) {
+nll <- function(par, y) {
+  mu       <- par[1]
+  sigma_1  <- par[2]
+  sigma_2  <- par[3]
+  p1       <- par[4]
   f <- p1 * dnorm(y, mean = mu, sd = sigma_1) +
     (1 - p1) * dnorm(y, mean = mu, sd = sigma_2)
   -sum(log(f))
 }
 
-mle <- optim(par = c(mu, sigma_1, sigma_2, p1), fn = nll, y = y)
+start <- c(mu, sigma_1, sigma_2, p1)
+ 
+mle <- optim(par = start, fn = nll, y = y)
 
-# print estimates
-mle$par
+# calculate P(s_t=2|y_t=0)
+mu_hat  <- mle$par[1]
+s1_hat  <- mle$par[2]
+s2_hat  <- mle$par[3]
+p1_hat  <- mle$par[4]
+
+y0 <- 0
+num <- (1 - p1_hat) * dnorm(y0, mean = mu_hat, sd = s2_hat)
+den <- p1_hat * dnorm(y0, mean = mu_hat, sd = s1_hat) + num
+prob <- as.numeric(num / den)
 
