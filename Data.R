9f78bd415ec5f503575bc69f359e2d6551086d9c
Timo Stuij <timo@MacBook-Pro.local>
2025-11-12 14:44:26 +0100

    data

--- Data.R
+++ Data.R
@@ -1,5 +1,11 @@
-getwd()               # moet de projectmap zijn
-# optioneel
-install.packages("here")
-here::here()          # geeft pad naar de projectroot
+# Data inladen 
+library(readr)
+df <- read_csv("growth_data.csv")
+
+T <- nrow(df)
+num_holdout <- 12
+
+# Alle data, behalve laatste twaalf voor estimation
+estimation_data <- df[1:(T - num_holdout), ]
+holdout_data <- df[(T - num_holdout + 1):T, ]
 
